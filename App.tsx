c191757bb1fc6f48b82d075dfbb4638e64e5002b
hcy6021 <81142151+hcy6021@users.noreply.github.com>
2022-11-04 23:10:58 +0900

    [mod]Root에 Navigator 추가함

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import {StatusBar} from 'react-native';
 import {RecoilRoot} from 'recoil';
+import Navigation from './navigation';
 
 export default function App() {
   return (
     <RecoilRoot>
       <StatusBar barStyle={'dark-content'} backgroundColor={'white'} />
+      <Navigation />
     </RecoilRoot>
   );
 }
