8e37da289afdcd8c34e169974cfb69784eded8a7
hcy6021 <81142151+hcy6021@users.noreply.github.com>
2022-11-04 23:24:19 +0900

    [rfc]prettier 적용

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
-import {StatusBar} from 'react-native';
-import {RecoilRoot} from 'recoil';
+import { StatusBar } from 'react-native';
+import { RecoilRoot } from 'recoil';
 import Navigation from './navigation';
 
 export default function App() {
-  return (
-    <RecoilRoot>
-      <StatusBar barStyle={'dark-content'} backgroundColor={'white'} />
-      <Navigation />
-    </RecoilRoot>
-  );
+	return (
+		<RecoilRoot>
+			<StatusBar barStyle={'dark-content'} backgroundColor={'white'} />
+			<Navigation />
+		</RecoilRoot>
+	);
 }
