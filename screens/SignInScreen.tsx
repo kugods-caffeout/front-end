f57adfa86511e58eaf96b218669549d0b127339e
hcy6021 <81142151+hcy6021@users.noreply.github.com>
2022-11-04 23:09:43 +0900

    [add]SignInScreen 추가함

---
+++ screens/SignInScreen.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import {View} from 'react-native';
+import {RootStackScreenProps} from '../types';
+
+export default function SignInScreen({
+  navigation,
+}: RootStackScreenProps<'SignIn'>) {
+  return <View></View>;
+}
