dff71d5536d6dd537aff24120afabf8c23f9cb51
김자헌 <kimjaheon@gimjaheon-ui-MacBookAir.local>
2022-11-07 18:20:33 +0900

    [add]컬러팔레트hex추가

---
+++ constants/Color.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default {
+    Brown :'#9C4A07',
+    Green :'#0D7C12',
+    Blue : '#3145FF',
+    Black: '#192533',
+    DeepGray: '#8B8B8B',
+    DarkGray: '#D9D9D9',
+    White: '#FFFEFE',
+}
