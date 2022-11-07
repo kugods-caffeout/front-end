fb556c80f54c9d1ddeb3997f3b06a37346d41c65
hcy6021 <81142151+hcy6021@users.noreply.github.com>
2022-11-08 03:52:43 +0900

    [mod]LightGray 색상 코드 추가

--- constants/Color.tsx
+++ constants/Color.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-
 export default {
-    Brown :'#9C4A07',
-    Green :'#0D7C12',
-    Blue : '#3145FF',
-    Black: '#192533',
-    DeepGray: '#8B8B8B',
-    DarkGray: '#D9D9D9',
-    White: '#FFFEFE',
-}
+	Brown: '#9C4A07',
+	Green: '#0D7C12',
+	Blue: '#3145FF',
+	Black: '#192533',
+	LightGray: '#F6F6F6',
+	DeepGray: '#8B8B8B',
+	DarkGray: '#D9D9D9',
+	White: '#FFFEFE',
+};
