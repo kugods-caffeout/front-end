fff1ef954765cfb3daff6cb8a026e4a4804252b7
MyoungJin <imannamj@gmail.com>
2022-11-12 21:02:18 +0900

    [mod]LightGray 배경 뷰 추가

--- screens/SignInScreen.tsx
+++ screens/SignInScreen.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
 import { View } from 'react-native';
 import { RootStackScreenProps } from '../types';
+import '../constants/Colors';
 
 export default function SignInScreen({
 	navigation,
 }: RootStackScreenProps<'SignIn'>) {
-	return <View></View>;
+	return <View style = {{ 
+		width : '100%',
+		height : '100%',
+		backgroundColor : 'LightGray',
+	}}></View>;
 }
