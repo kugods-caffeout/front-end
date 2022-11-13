7c492d8c854671017632b8621b207bdd7a238140
hcy6021 <81142151+hcy6021@users.noreply.github.com>
2022-11-13 21:57:32 +0900

    [add]음료 검색바 추가

--- screens/SearchDrinkScreen.tsx
+++ screens/SearchDrinkScreen.tsx
@@ -1,5 +1,12 @@
 import * as React from 'react';
-import { View, Dimensions, FlatList, Text, Pressable } from 'react-native';
+import {
+	View,
+	Dimensions,
+	FlatList,
+	Text,
+	Pressable,
+	TextInput,
+} from 'react-native';
 import { RootStackScreenProps } from '../types';
 import Colors from '../constants/Colors';
 
@@ -9,6 +16,17 @@ import { DrinkList } from '../data/DrinkList';
 export default function SearchDrinkScreen({
 	navigation,
 }: RootStackScreenProps<'SearchDrink'>) {
+	const [drinkKeyword, setDrinkKeyword] = React.useState('');
+
+	function searchedDrinkList(drinkList: typeof DrinkList, keyword: string) {
+		if (keyword === '') {
+			return drinkList;
+		} else {
+			return drinkList.filter((drink: any) =>
+				drink.drinkName.includes(keyword),
+			);
+		}
+	}
 	return (
 		<View
 			style={{
@@ -17,6 +35,31 @@ export default function SearchDrinkScreen({
 				backgroundColor: Colors.LightGray,
 			}}
 		>
+			<View
+				style={{
+					width: Dimensions.get('window').width,
+					height: 56,
+					backgroundColor: Colors.White,
+					justifyContent: 'center',
+					alignItems: 'center',
+					marginBottom: 2,
+				}}
+			>
+				<View
+					style={{
+						width: 358,
+						height: 44,
+						backgroundColor: Colors.LightGray,
+						borderRadius: 15,
+					}}
+				>
+					<TextInput
+						value={drinkKeyword}
+						onChangeText={(keyword: string) => setDrinkKeyword(keyword)}
+					></TextInput>
+				</View>
+			</View>
+
 			<FlatList
 				renderItem={({ item }) => (
 					<DrinkItem
@@ -25,7 +68,7 @@ export default function SearchDrinkScreen({
 						caffeine={item.caffeine}
 					/>
 				)}
-				data={DrinkList}
+				data={searchedDrinkList(DrinkList, drinkKeyword)}
 			/>
 		</View>
 	);
