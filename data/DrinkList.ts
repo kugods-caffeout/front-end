03b48b609907c092332876ad2a7c56c09b2c449e
hcy6021 <81142151+hcy6021@users.noreply.github.com>
2022-11-13 21:41:55 +0900

    [add]음료 목록 FlatList의 data용 mock data 추가

---
+++ data/DrinkList.ts
@@ -0,0 +1,22 @@
+export const DrinkList = [
+    {
+        brand: '스타벅스',
+        drinkName: '돌체 콜드 브루',
+        caffeine: 155,
+    },
+    {
+        brand: '스타벅스',
+        drinkName: '스타벅스 돌체 라떼',
+        caffeine: 150,
+    },
+    {
+        brand: '스타벅스',
+        drinkName: '아이스 스타벅스 돌체 라떼',
+        caffeine: 150,
+    },
+    {
+        brand: '스타벅스',
+        drinkName: '돌체 블랙 밀크 티 ',
+        caffeine: 60,
+    },
+]
