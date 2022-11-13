92f4e3870cf6675f166f3623ce836d759774b406
hcy6021 <81142151+hcy6021@users.noreply.github.com>
2022-11-13 21:43:14 +0900

    [rfc]음료 목록 FlatList 리팩토링

--- screens/SearchDrinkScreen.tsx
+++ screens/SearchDrinkScreen.tsx
@@ -3,6 +3,9 @@ import { View, Dimensions, FlatList, Text, Pressable } from 'react-native';
 import { RootStackScreenProps } from '../types';
 import Colors from '../constants/Colors';
 
+import DrinkItem from '../component/DrinkItem';
+import { DrinkList } from '../data/DrinkList';
+
 export default function SearchDrinkScreen({
 	navigation,
 }: RootStackScreenProps<'SearchDrink'>) {
@@ -16,83 +19,14 @@ export default function SearchDrinkScreen({
 		>
 			<FlatList
 				renderItem={({ item }) => (
-					<Pressable
-						style={({ pressed }) => ({
-							opacity: pressed ? 0.5 : 1,
-							width: Dimensions.get('window').width,
-							height: 52,
-							backgroundColor: Colors.White,
-							flexDirection: 'row',
-							alignItems: 'center',
-							paddingHorizontal: 16,
-							marginBottom: 2,
-						})}
-					>
-						<View
-							style={{
-								width: Dimensions.get('window').width * 0.8,
-								flexDirection: 'row',
-								justifyContent: 'space-between',
-							}}
-						>
-							<Text
-								style={{
-									fontSize: 16,
-									color: Colors.Black,
-								}}
-							>
-								{item.brand}
-							</Text>
-							<View
-								style={{
-									flexDirection: 'row',
-									justifyContent: 'space-between',
-									width: Dimensions.get('window').width * 0.6,
-								}}
-							>
-								<Text
-									style={{
-										fontSize: 16,
-										color: Colors.Black,
-									}}
-								>
-									{item.drinkName}
-								</Text>
-								<Text
-									style={{
-										fontSize: 16,
-										color: Colors.Black,
-									}}
-								>
-									{item.caffeine}mg
-								</Text>
-							</View>
-						</View>
-					</Pressable>
+					<DrinkItem
+						brand={item.brand}
+						drinkName={item.drinkName}
+						caffeine={item.caffeine}
+					/>
 				)}
-				data={[
-					{
-						brand: '스타벅스',
-						drinkName: '돌체 콜드 브루',
-						caffeine: 155,
-					},
-					{
-						brand: '스타벅스',
-						drinkName: '스타벅스 돌체 라떼',
-						caffeine: 150,
-					},
-					{
-						brand: '스타벅스',
-						drinkName: '아이스 스타벅스 돌체 라떼',
-						caffeine: 150,
-					},
-					{
-						brand: '스타벅스',
-						drinkName: '돌체 블랙 밀크 티 ',
-						caffeine: 60,
-					},
-				]}
-			></FlatList>
+				data={DrinkList}
+			/>
 		</View>
 	);
 }
