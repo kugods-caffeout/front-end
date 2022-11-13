5ebc260f37ea95932ebc76d5c8ce72308c81a835
hcy6021 <81142151+hcy6021@users.noreply.github.com>
2022-11-13 22:10:33 +0900

    [mod]음료 목록 mock data 수정

--- data/DrinkList.ts
+++ data/DrinkList.ts
@@ -19,4 +19,34 @@ export const DrinkList = [
         drinkName: '돌체 블랙 밀크 티 ',
         caffeine: 60,
     },
+    {
+        brand: '공차',
+        drinkName: '골든펄 돌체 밀크티',
+        caffeine: 58,
+    },
+    {
+        brand: '공차',
+        drinkName: '골든펄 돌체 밀크티 스무디',
+        caffeine: 34,
+    },
+    {
+        brand: '공차',
+        drinkName: '돌체 카페라떼(아이스/L)',
+        caffeine: 93,
+    },
+    {
+        brand: '공차',
+        drinkName: '돌체 카페라떼(아이스/J)',
+        caffeine: 221,
+    },
+    {
+        brand: '공차',
+        drinkName: '돌체 카페라떼(핫/L)',
+        caffeine: 145,
+    },
+    {
+        brand: '공차',
+        drinkName: '돌체 카페라떼(핫/J)',
+        caffeine: 246,
+    }
 ]
