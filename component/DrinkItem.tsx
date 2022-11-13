68cb73fc90fcb97fca280bba2eeb100f261985c1
hcy6021 <81142151+hcy6021@users.noreply.github.com>
2022-11-13 21:37:07 +0900

    [add]음료 목록 FlatList의 renderItem용 컴포넌트 추가

---
+++ component/DrinkItem.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import { View, Dimensions, FlatList, Text, Pressable } from 'react-native';
+import Colors from '../constants/Colors';
+
+export default function DrinkItem(props: {
+	brand: string;
+	drinkName: string;
+	caffeine: number;
+}) {
+	return (
+		<Pressable
+			style={({ pressed }) => ({
+				opacity: pressed ? 0.5 : 1,
+				width: Dimensions.get('window').width,
+				height: 52,
+				backgroundColor: Colors.White,
+				flexDirection: 'row',
+				alignItems: 'center',
+				paddingHorizontal: 16,
+				marginBottom: 2,
+			})}
+		>
+			<View
+				style={{
+					width: Dimensions.get('window').width * 0.8,
+					flexDirection: 'row',
+					justifyContent: 'space-between',
+				}}
+			>
+				<Text
+					style={{
+						fontSize: 16,
+						color: Colors.Black,
+					}}
+				>
+					{props.brand}
+				</Text>
+				<View
+					style={{
+						flexDirection: 'row',
+						justifyContent: 'space-between',
+						width: Dimensions.get('window').width * 0.6,
+					}}
+				>
+					<Text
+						style={{
+							fontSize: 16,
+							color: Colors.Black,
+						}}
+					>
+						{props.drinkName}
+					</Text>
+					<Text
+						style={{
+							fontSize: 16,
+							color: Colors.Black,
+						}}
+					>
+						{props.caffeine}mg
+					</Text>
+				</View>
+			</View>
+		</Pressable>
+	);
+}
