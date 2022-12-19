8f0d9be9d47cbf47a90a3e2e187efb0d4a326dc8
Chaeyoung Ha <81142151+ChaeyoungHa@users.noreply.github.com>
2022-12-19 23:18:39 +0900

    [mod]BottomTabBar 추가 & HomeScreen 추가 및 tabBarIcon 적용

--- navigation.tsx
+++ navigation.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { RootStackParamList } from './types';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { RootStackParamList, RootTabParamList } from './types';
 
 import SearchDrinkScreen from './screens/SearchDrinkScreen';
 import DrinkDetailScreen from './screens/DrinkDetailScreen';
+import HomeScreen from './screens/HomeScreen';
 
+import HomeScreenIconSelected from './assets/homeScreenIconSelected.svg';
+import HomeScreenIconUnselected from './assets/homeScreenIconUnselected.svg';
+import SearchDrinkScreenIconSelected from './assets/searchDrinkScreenIconSelected.svg';
+import SearchDrinkScreenIconUnselected from './assets/searchDrinkScreenIconUnselected.svg';
+import Colors from './constants/Colors';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 export default function Navigation() {
 	return (
 		<NavigationContainer>
@@ -20,19 +28,54 @@ function RootNavigator() {
 	return (
 		<Stack.Navigator>
 			<Stack.Screen
-				name='DrinkDetail'
-				component={DrinkDetailScreen}
-				options={{
-				 	headerShown: false,
-				}}
-			/>
-			<Stack.Screen
-				name='SearchDrink'
-				component={SearchDrinkScreen}
-				// options={{
-				// 	headerShown: false,
-				// }}
+				name='Root'
+				component={BottomTabNavigator}
+				options={{ headerShown: false, animation: 'fade' }}
 			/>
 		</Stack.Navigator>
+	);
+}
+
+const BottomTab = createBottomTabNavigator<RootTabParamList>();
+
+function BottomTabNavigator() {
+	console.log();
+	return (
+		<BottomTab.Navigator
+			initialRouteName='Home'
+			screenOptions={{
+				tabBarShowLabel: false,
+				tabBarStyle: {
+					backgroundColor: Colors.DarkBrown,
+					borderTopWidth: 0,
+					height: 48 + useSafeAreaInsets().bottom,
+					paddingBottom: useSafeAreaInsets().bottom,
+				},
+				tabBarHideOnKeyboard: true,
+				headerShown: false,
+			}}
+		>
+			<BottomTab.Screen
+				name='Home'
+				component={HomeScreen}
+				options={{
+					tabBarIcon: ({ focused }) =>
+						focused ? <HomeScreenIconSelected /> : <HomeScreenIconUnselected />,
+				}}
+			/>
+			<BottomTab.Screen name='DrinkDetail' component={DrinkDetailScreen} />
+			<BottomTab.Screen
+				name='SearchDrink'
+				component={SearchDrinkScreen}
+				options={{
+					tabBarIcon: ({ focused }) =>
+						focused ? (
+							<SearchDrinkScreenIconSelected />
+						) : (
+							<SearchDrinkScreenIconUnselected />
+						),
+				}}
+			/>
+		</BottomTab.Navigator>
 	);
 }
