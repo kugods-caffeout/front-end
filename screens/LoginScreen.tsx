2d1dfef266b4cf81e60ddc08f12c6b618b24eee2
hcy6021 <81142151+hcy6021@users.noreply.github.com>
2023-01-07 23:37:19 +0900

    [mod]로그인 버튼 추가

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,8 +1,65 @@
 import * as React from 'react';
-import { View, FlatList, Pressable, TextInput, StyleSheet } from 'react-native';
+import {
+	View,
+	FlatList,
+	Pressable,
+	TextInput,
+	StyleSheet,
+	Text,
+} from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import Colors from '../constants/Colors';
+import Dimensions from '../constants/Dimensions';
 import { RootStackScreenProps } from '../types';
 export default function LoginScreen({
 	navigation,
 }: RootStackScreenProps<'Login'>) {
-	return <View></View>;
+	return (
+		<View
+			style={[
+				styles.topContainer,
+				{
+					paddingTop: useSafeAreaInsets().top,
+					paddingBottom: useSafeAreaInsets().bottom,
+				},
+			]}
+		>
+			<Text
+				style={{
+					color: Colors.Black,
+					fontWeight: 'bold',
+					fontSize: 30,
+				}}
+			>
+				로그인 화면
+			</Text>
+			<Pressable
+				style={({ pressed }) => [
+					{
+						opacity: pressed ? 0.5 : 1,
+					},
+					styles.loginButton,
+				]}
+			>
+				<Text>로그인</Text>
+			</Pressable>
+		</View>
+	);
 }
+
+const styles = StyleSheet.create({
+	topContainer: {
+		width: Dimensions.width * 390,
+		height: Dimensions.height * 844,
+		backgroundColor: Colors.White,
+		alignItems: 'center',
+	},
+	loginButton: {
+		width: Dimensions.width * 358,
+		height: Dimensions.height * 44,
+		borderRadius: 30,
+		backgroundColor: '#FEE500',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+});
