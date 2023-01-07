7ab9f843a7f1b4bb31bf0e9cd88081cf0a894667
hcy6021 <81142151+hcy6021@users.noreply.github.com>
2023-01-07 21:28:47 +0900

    [ADD]하단탭바 없애고 LoginScreen 추가

---
+++ screens/LoginScreen.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { View, FlatList, Pressable, TextInput, StyleSheet } from 'react-native';
+import { RootStackScreenProps } from '../types';
+export default function LoginScreen({
+	navigation,
+}: RootStackScreenProps<'Login'>) {
+	return <View></View>;
+}
