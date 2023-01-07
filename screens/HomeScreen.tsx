d43f8f588ab734619aa22a4714be90909158d3ff
hcy6021 <81142151+hcy6021@users.noreply.github.com>
2023-01-07 21:29:31 +0900

    [rft]오타 수정 및 prettier 적용

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,4 +1,12 @@
-import { StyleSheet, View, Text, ScrollView, Pressable, SafeAreaView, Animated } from 'react-native';
+import {
+	StyleSheet,
+	View,
+	Text,
+	ScrollView,
+	Pressable,
+	SafeAreaView,
+	Animated,
+} from 'react-native';
 import Colors from '../constants/Colors';
 import Dimensions from '../constants/Dimensions';
 import { RootStackScreenProps } from '../types';
@@ -10,108 +18,173 @@ import RightArrow from '../assets/main-right-arrow.svg';
 import { useEffect, useRef, useState } from 'react';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
-export default function DrinkDetailScreen({
+export default function HomeScreen({
 	navigation,
 	route,
 }: RootStackScreenProps<'Home'>) {
-	const loaderValue = useRef(new Animated.Value(0)).current
+	const loaderValue = useRef(new Animated.Value(0)).current;
 	const [isDatePickerVisible, setDatePickerVisible] = useState(false);
 	const [initalDate, setInitalDate] = useState(new Date());
 
 	const ShowDatePicker = () => {
 		setDatePickerVisible(true);
-	}
-	const handleConfirm = (date:Date) => {
+	};
+	const handleConfirm = (date: Date) => {
 		hideDatePicker();
 		setInitalDate(date);
-	}
+	};
 	const hideDatePicker = () => {
 		setDatePickerVisible(false);
-	}
+	};
 
 	const TomorrowDate = async () => {
 		let promise = new Promise((resolve, reject) => {
 			const current = initalDate;
 			current.setDate(current.getDate() + 1);
 			resolve(current);
-		})
-		let result:any= await promise;
+		});
+		let result: any = await promise;
 		setInitalDate(result);
-	}
+	};
 
-	const Yesterday = async() => {
+	const Yesterday = async () => {
 		let promise = new Promise((resolve, reject) => {
 			const current = initalDate;
 			current.setDate(current.getDate() - 1);
 			resolve(current);
-		})
-		let result:any= await promise;
+		});
+		let result: any = await promise;
 		setInitalDate(result);
-	}
+	};
 
 	const load = (amount: number) => {
 		Animated.timing(loaderValue, {
-			toValue: (amount/370)*100, //amount들어가야함
+			toValue: (amount / 370) * 100, //amount들어가야함
 			duration: 500,
 			useNativeDriver: false,
-		}
-		).start();
-	}
+		}).start();
+	};
 
 	const width = loaderValue.interpolate({
 		inputRange: [0, 100],
-		outputRange: ["0%", "100%"],
+		outputRange: ['0%', '100%'],
 		extrapolate: 'clamp',
-	})
+	});
 
 	useEffect(() => {
 		load(120); //amount
-	}, [])
-	
+	}, []);
+
 	return (
 		<SafeAreaView style={styles.topContainer}>
-			<View style={{ width: Dimensions.width * 390, height: Dimensions.height * 44,paddingHorizontal:10,backgroundColor: Colors.White, justifyContent: 'space-between', alignItems: 'center', flexDirection: 'row' }}>
+			<View
+				style={{
+					width: Dimensions.width * 390,
+					height: Dimensions.height * 44,
+					paddingHorizontal: 10,
+					backgroundColor: Colors.White,
+					justifyContent: 'space-between',
+					alignItems: 'center',
+					flexDirection: 'row',
+				}}
+			>
 				<LeftArrow onPress={Yesterday} />
-				<Pressable onPress={ShowDatePicker} style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-					<Text style={{fontSize:24,fontWeight:'bold'}}>{initalDate.getMonth()+1}.{initalDate.getDate()}</Text>
+				<Pressable
+					onPress={ShowDatePicker}
+					style={{
+						flexDirection: 'row',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}
+				>
+					<Text style={{ fontSize: 24, fontWeight: 'bold' }}>
+						{initalDate.getMonth() + 1}.{initalDate.getDate()}
+					</Text>
 					<DownArrow onPress={ShowDatePicker}></DownArrow>
 				</Pressable>
 				<RightArrow onPress={TomorrowDate} />
 			</View>
 			<DateTimePickerModal
-        	isVisible={isDatePickerVisible}
-        	mode="date"
-        	onConfirm={handleConfirm}
-			onCancel={hideDatePicker}
-			display={'inline'}
-			locale={'ko'}
-			confirmTextIOS={'확인'}
-			cancelTextIOS={'취소'}
-			accentColor={Colors.Brown}
-			date={initalDate}	
-      		/>
-			<View style={{width:Dimensions.width * 390, height:Dimensions.height*1, backgroundColor:Colors.LightGray}}></View>
-			<View style={{ width: Dimensions.width * 390, height: Dimensions.height * 164,justifyContent:'center',alignItems:'center',backgroundColor:Colors.White}}>
-				<View style={{flexDirection:'row',justifyContent:'center',marginTop: Dimensions.height * 16}}>
-					<Text style={{ fontSize: 40, fontWeight: 'bold' , color:Colors.Green}}>120</Text>
+				isVisible={isDatePickerVisible}
+				mode='date'
+				onConfirm={handleConfirm}
+				onCancel={hideDatePicker}
+				display={'inline'}
+				locale={'ko'}
+				confirmTextIOS={'확인'}
+				cancelTextIOS={'취소'}
+				accentColor={Colors.Brown}
+				date={initalDate}
+			/>
+			<View
+				style={{
+					width: Dimensions.width * 390,
+					height: Dimensions.height * 1,
+					backgroundColor: Colors.LightGray,
+				}}
+			></View>
+			<View
+				style={{
+					width: Dimensions.width * 390,
+					height: Dimensions.height * 164,
+					justifyContent: 'center',
+					alignItems: 'center',
+					backgroundColor: Colors.White,
+				}}
+			>
+				<View
+					style={{
+						flexDirection: 'row',
+						justifyContent: 'center',
+						marginTop: Dimensions.height * 16,
+					}}
+				>
+					<Text
+						style={{ fontSize: 40, fontWeight: 'bold', color: Colors.Green }}
+					>
+						120
+					</Text>
 					<Text style={{ fontSize: 40, fontWeight: 'bold' }}>/</Text>
 					<Text style={{ fontSize: 40, fontWeight: 'bold' }}>370</Text>
 				</View>
-				<View style={{width: Dimensions.width * 319, height: Dimensions.height*11, backgroundColor:Colors.DarkGray, borderRadius:30,marginTop:36}}>
-				<Animated.View
+				<View
 					style={{
-						backgroundColor: Colors.DarkBrown,
-						width,
+						width: Dimensions.width * 319,
+						height: Dimensions.height * 11,
+						backgroundColor: Colors.DarkGray,
+						borderRadius: 30,
+						marginTop: 36,
+					}}
+				>
+					<Animated.View
+						style={{
+							backgroundColor: Colors.DarkBrown,
+							width,
 							height: Dimensions.height * 11,
 							borderRadius: 30,
-						
-						}}>
-				</Animated.View>
+						}}
+					></Animated.View>
 				</View>
-				<Text style={{fontSize:14,marginTop:14}}>"이 정도면 적당해요!"</Text>
+				<Text style={{ fontSize: 14, marginTop: 14 }}>
+					"이 정도면 적당해요!"
+				</Text>
 			</View>
-			<Pressable style={{ position: 'absolute', right: 34, bottom: 84, height: 60, width: 60, borderRadius: 50, backgroundColor: Colors.Brown, justifyContent: 'center', alignItems: 'center' }}
-			onPress={()=>{navigation.navigate('SearchDrink')}}> 
+			<Pressable
+				style={{
+					position: 'absolute',
+					right: 34,
+					bottom: 84,
+					height: 60,
+					width: 60,
+					borderRadius: 50,
+					backgroundColor: Colors.Brown,
+					justifyContent: 'center',
+					alignItems: 'center',
+				}}
+				onPress={() => {
+					navigation.navigate('SearchDrink');
+				}}
+			>
 				<PlusIcon></PlusIcon>
 			</Pressable>
 		</SafeAreaView>
